b2a4ba88fce3b2651cedb12c34a3c130ae12e610
aadityaadyot <aadityaadyot@gmail.com>
2024-03-01 12:16:04 +0530

    updated folders

--- Xray/cloud_storage/S3_operations.py
+++ Xray/cloud_storage/S3_operations.py
@@ -4,14 +4,28 @@ from Xray.exception import XRayException
 
 class S3Operation:
 
-    def sync_folder_to_s3()-->None:
+    def sync_folder_to_s3(
+        self, folder: str, bucket_name: str, bucket_folder_name: str
+    ) -> None:
         try:
-            pass
-        except Exception as e:
-            raise XRayException(e,sys)
+            command: str = (
+                f"aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}/ "
+            )
 
-    def sync_folder_from_s3()--> None:
-        try:
-            pass
+            os.system(command)
+
         except Exception as e:
-            raise XRayException(e,sys)
+            raise XRayException(e, sys)
+
+    def sync_folder_from_s3(
+        self, folder: str, bucket_name: str, bucket_folder_name: str
+    ) -> None:
+        try:
+            command: str = (
+                f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/ {folder} "
+            )
+
+            os.system(command)
+
+        except Exception as e:
+            raise XRayException(e, sys)
